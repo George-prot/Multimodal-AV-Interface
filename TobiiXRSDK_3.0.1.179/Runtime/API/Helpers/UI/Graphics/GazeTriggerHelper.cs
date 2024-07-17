c0b30b5b0bb983038813d305296d4df49def7930
George2 <aek_george@hotmail.com>
2024-07-17 17:35:59 +0300

    second

---
+++ TobiiXRSDK_3.0.1.179/Runtime/API/Helpers/UI/Graphics/GazeTriggerHelper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.XR;
+
+namespace Helper
+{
+
+    public class GazeTriggerHelper : MonoBehaviour
+    {
+
+        public Transform oldBtnCord;
+        public float timeElapsed = 0f;
+
+    }
+
+}
